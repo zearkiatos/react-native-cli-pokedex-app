bb76b56be25123ba29295f602a024aff900f1558
zearkiatos <caprilespe@outlook.com>
2022-06-06 20:04:00 -0400

    User Data and logout

--- src/components/Auth/UserData/index.js
+++ src/components/Auth/UserData/index.js
@@ -1,14 +1,63 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-
+import {View, StyleSheet, Text, Button} from 'react-native';
+import useAuth from '../../../hooks/useAuth';
 const UserData = () => {
+  const {auth, logout} = useAuth();
   return (
-    <View>
-      <Text>User Data</Text>
+    <View style={styles.content}>
+      <View style={styles.titleBlock}>
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.title}>{`${auth.firstName} ${auth.lastName}`}</Text>
+      </View>
+      <View style={styles.dataContent}>
+        <ItemMenu title="Name" text={`${auth.firstName} ${auth.lastName}`} />
+        <ItemMenu title="Username" text={auth.username} />
+        <ItemMenu title="Email" text={auth.email} />
+        <ItemMenu title="Total Favorites" text={0} />
+      </View>
+      <Button title="Logout" onPress={logout} style={styles.logout} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const ItemMenu = ({title, text}) => {
+  return (
+    <View style={styles.itemMenu}>
+      <Text style={styles.itemMenuTitle}>{title}: </Text>
+      <Text>{text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  titleBlock: {
+    marginBottom: 30,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+  dataContent: {
+    marginBottom: 20,
+  },
+  itemMenu: {
+    flexDirection: 'row',
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: '#CFCFCF',
+  },
+  itemMenuTitle: {
+    fontWeight: 'bold',
+    paddingRight: 10,
+    width: 120,
+  },
+  logout: {
+    paddingTop: 20,
+  },
+});
 
 export default UserData;
