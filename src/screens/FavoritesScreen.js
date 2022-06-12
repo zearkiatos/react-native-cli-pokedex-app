8f50e402e4dcb920c61522a21c837fb236604565
zearkiatos <caprilespe@outlook.com>
2022-06-11 22:26:17 -0400

    Pokemon favorites list

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,19 +1,41 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, Text, Button} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {useFocusEffect} from '@react-navigation/native';
+import PokemonList from '../components/PokemonList';
 import {get} from '../client/favoriteClient';
+import {getPokemonById} from '../client/pokemonClient';
+import useAuth from '../hooks/useAuth';
 
 const FavoritesScreen = () => {
-  const [favorites, setFavorites] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+  const {auth} = useAuth();
   const getFavorites = async () => {
-    const data = await get();
-    setFavorites(data);
-    console.log(data);
+    const favorites = await get();
+
+    const pokemonList = [];
+    for await (let id of favorites) {
+      const pokemonData = await getPokemonById(id);
+      pokemonList.push({
+        id: pokemonData.id,
+        name: pokemonData.name,
+        types: pokemonData.types,
+        order: pokemonData.order,
+        image: pokemonData.sprites.other['official-artwork'].front_default,
+      });
+    }
+    setPokemons(pokemonList);
   };
-  return (
-    <SafeAreaView>
-      <Text>Favorites</Text>
-      <Button title="Get Favorites" onPress={getFavorites} />
-    </SafeAreaView>
+
+  useFocusEffect(
+    useCallback(() => {
+      getFavorites();
+    }, [auth]),
+  );
+
+  return !auth ? (
+    <Text> User not authenticated</Text>
+  ) : (
+    <PokemonList pokemons={pokemons} />
   );
 };
 
